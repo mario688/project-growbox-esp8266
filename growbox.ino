2b3514a9f71431a057f7c57358f7d64e00039e4c
mario688 <gliwamario@gmail.com>
2021-11-24 18:08:53 +0100

    Added brightness sensor and PCF8574 expander

--- growbox.ino
+++ growbox.ino
@@ -1,22 +1,26 @@
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>  
+#include <Wire.h>
+#include <BH1750.h>
 #include <FirebaseArduino.h>
 #include "DHT.h"
+#include "PCF8574.h"
 
 
-
-#define DHTPIN D2
-const byte interruptPin = 12;
+#define DHTPIN D4
 #define DHTTYPE DHT22 
 #define LEDPIN 13
 #define trigPin D3
 #define echoPin D0
+const byte interruptPin = 12;
 unsigned long previousTimeWater = 0;
 unsigned long previousTimeDHT22 = 0;
 const String growbox_ID = "GROWBOX123";
 long duration;
 int distance;
 DHT dht(DHTPIN, DHTTYPE);
+PCF8574 pcf8574(0x20);
+BH1750 lightMeter(0x23);
 WiFiManager wifiManager;
 //elapsedMillis timeElapsed;
 void ICACHE_RAM_ATTR resetDevice(){
@@ -28,14 +32,29 @@ void ICACHE_RAM_ATTR resetDevice(){
 void setup() {
    Serial.begin(9600);
    dht.begin();
+   Wire.begin();
    pinMode(interruptPin,INPUT_PULLUP);
    pinMode(trigPin, OUTPUT); 
    pinMode(echoPin, INPUT);
    pinMode(LEDPIN,OUTPUT);
    digitalWrite(LEDPIN, LOW);  
    attachInterrupt(digitalPinToInterrupt(interruptPin), resetDevice, RISING);
-  
-  
+  for(int i=0;i<8;i++)
+  {
+    pcf8574.pinMode(i, OUTPUT,LOW);
+  }
+  if (lightMeter.begin()) {
+    Serial.println(F("BH1750 ok"));
+  }
+  else {
+    Serial.println(F("Error initialising BH1750"));
+  }
+  Serial.print("Init pcf8574...");
+  if (pcf8574.begin()){
+    Serial.println("OK");
+  }else{
+    Serial.println("KO");
+  }
   wifiManager.autoConnect("GrowBox");
 //  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 //  Serial.print("connecting");
@@ -50,7 +69,14 @@ void setup() {
  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
  
 }
-
+void brightness(){
+  if (lightMeter.measurementReady()) {
+    float lux = lightMeter.readLightLevel();
+    Serial.print("Light: ");
+    Serial.print(lux);
+    Serial.println(" lx");
+  }
+}
 void waterLevel()
 {
   
@@ -70,11 +96,13 @@ void waterLevel()
     percentageLvlWater = 0;
   }
   String water_level = String(percentageLvlWater);
-  Firebase.setString("devices/"+growbox_ID+"/sensors/water", water_level);
-  if (Firebase.failed()) {
-      Serial.print("Can't set Humidity");
-      Serial.println(Firebase.error());
-  }
+  Serial.print("Water Level: ");
+  Serial.println(water_level);
+//Firebase.setString("devices/"+growbox_ID+"/sensors/water", water_level);
+//  if (Firebase.failed()) {
+//      Serial.print("Can't set Humidity");
+//      Serial.println(Firebase.error());
+//  }
  
 }
 void soliMoisture(){
@@ -97,33 +125,43 @@ void soliMoisture(){
 void getValueFromSensors(){
   float temp = dht.readTemperature();
   float hum = dht.readHumidity();
-  Firebase.setFloat("devices/"+growbox_ID+"/sensors/temp", temp);
-  if (Firebase.failed()) {
-      Serial.print("Can't set Humidity");
-      Serial.println(Firebase.error());
-  }
-  Firebase.setFloat("devices/"+growbox_ID+"/sensors/hum", hum);
-   if (Firebase.failed()) {
-      Serial.print("Can't set Humidity");
-      Serial.println(Firebase.error());
-  }
+  Serial.print("Temp: ");
+  Serial.print(temp);
+  Serial.print("  ");
+  Serial.print("Hum: ");
+  Serial.print(hum);
+//  Firebase.setFloat("devices/"+growbox_ID+"/sensors/temp", temp);
+//  if (Firebase.failed()) {
+//      Serial.print("Can't set Humidity");
+//      Serial.println(Firebase.error());
+//  }
+//  Firebase.setFloat("devices/"+growbox_ID+"/sensors/hum", hum);
+//   if (Firebase.failed()) {
+//      Serial.print("Can't set Humidity");
+//      Serial.println(Firebase.error());
+//  }
 }
 
 
 
 void loop() {
 unsigned long currentTime = millis();
-
-
-if(currentTime-previousTimeWater >= 60000){
+  pcf8574.digitalWrite(P0, HIGH);
+  delay(200);
+  pcf8574.digitalWrite(P0, LOW);
+  delay(200);
+//
+if(currentTime-previousTimeWater >= 5000){
   waterLevel();
+  brightness();
+  getValueFromSensors();
   previousTimeWater = currentTime;
 }
-if(currentTime-previousTimeDHT22 >= 60000){
-  soliMoisture();
-    getValueFromSensors();
-  previousTimeDHT22 = currentTime;
-}
+//if(currentTime-previousTimeDHT22 >= 60000){
+//  soliMoisture();
+//    getValueFromSensors();
+//  previousTimeDHT22 = currentTime;
+//}
 
 
 //getValueFromSensors();
